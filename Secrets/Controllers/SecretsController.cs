ec257e2812f271c9afde5950d2b705103b9426a0
Robert Hardy <helloarchy@gmail.com>
2021-12-04 16:40:02 +0000

    Access secrets in controllers

--- Secrets/Controllers/SecretsController.cs
+++ Secrets/Controllers/SecretsController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Secrets.Models;
 
 namespace Secrets.Controllers
 {
@@ -10,11 +13,26 @@ namespace Secrets.Controllers
     [ApiController]
     public class SecretsController : ControllerBase
     {
+        public IConfiguration Configuration { get; }
+
+        public SecretsController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // GET api/values
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Ok");
+            // var connectionString = Configuration["ConnectionString"];
+            // var facebook = Configuration.GetSection("Facebook").Get<Facebook>();
+            // return Ok($"Facebook secret = {facebook.AppSecret}, Facebook id = {facebook.AppId}");
+
+            var myConnection = new SqlConnectionStringBuilder("Server=.;Database=SecretsApp;User=hardy")
+            {
+                Password = Configuration["DbPassword"]
+            };
+            return Ok(myConnection.ToString());
         }
     }
 }
